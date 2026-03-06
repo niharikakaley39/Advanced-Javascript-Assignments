64796c92992f636ec483c6bb061f7566ffbc725a
niharikakaley39 <104619935+niharikakaley39@users.noreply.github.com>
2026-03-06 13:22:54 +0530

    done

--- callbacks/easy/once.js
+++ callbacks/easy/once.js
@@ -7,7 +7,23 @@
 // the callback with the same result (or error) from the first invocation.
 
 function once(fn) {
-
+let call = false;
+ let storedError = null;
+ let storedData = null;
+ return function(...args){
+    const callback = args.pop();
+    if(call){
+        callback(storedError, storedData);
+        return;
+    }
+    call = true;
+    fn(...args, (err ,data) => {
+        storedError = err;
+        storedData = data;
+        callback(err, data);
+    });
+ };
+}
 }
 
 module.exports = once;
