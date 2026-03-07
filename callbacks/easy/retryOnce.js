f473e934f4f28631d793f77f0d7210eab644d261
niharikakaley39 <104619935+niharikakaley39@users.noreply.github.com>
2026-03-07 14:24:08 +0530

    done

--- callbacks/easy/retryOnce.js
+++ callbacks/easy/retryOnce.js
@@ -7,7 +7,21 @@
 
 
 function retryOnce(fn) {
-
+return function(...args){
+     const callback = args.pop();
+     fn(...args,(err,data) =>{
+       if(!err){
+         return callback(null,data);
+       }
+       fn(...args,(err2,data) =>{
+         if(!err2){
+           callback(null, data2);
+         }
+         else{
+              callback(err2, null);
+         }
+       });
+     });
+   }
 }
-
 module.exports = retryOnce;
